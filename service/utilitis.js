602b173636595d44054ff3f4c5ba70e3c27616bd
Jensaboi <jens.lundin1005@gmail.com>
2023-10-08 05:51:27 +0200

    h

---
+++ service/utilitis.js
@@ -0,0 +1,27 @@
+import {items, list1, list2, moveToList1, moveToList2, renderLists} from "../script.js"
+
+export function changeList(item){
+    if(items.includes(item)){
+      let itemToRemove = items.indexOf(item)
+      items.splice(itemToRemove,1)
+  
+    }else{
+      items.push(item)
+    }
+  }
+
+export function removeCommonItems(removeFrom, checkWith){
+    return removeFrom.filter(item => !checkWith.includes(item));
+  }
+
+export function initBtns(){
+    document.getElementById("move-to-list2").addEventListener("click", () => {
+      moveToList2()
+      renderLists()
+    })
+  
+    document.getElementById("move-to-list1").addEventListener("click", () => {
+      moveToList1()
+      renderLists()
+    })
+  }
